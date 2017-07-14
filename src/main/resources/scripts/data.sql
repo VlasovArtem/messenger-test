5ce4e91ed73f0c4c4d3505b36da16b9f7dc37ab0
Vlasov Artem <avlasov@selectica.com>
2017-07-14 17:55:22 +0300

    Add sql schema and data scripts

---
+++ src/main/resources/scripts/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user VALUES (1, NOW(), null, 'test_first_name', 'test_last_name', 'test@mail.com', 'MALE');
+INSERT INTO message VALUES (1, NOW(), null, 'test_title', 'test_body', 1);
+INSERT INTO comment VALUES (1, NOW(), null, 'test_comment', 1, 1);
+INSERT INTO like_table VALUES (1, NOW(), null, 1, 1);
+INSERT INTO share VALUES (1, NOW(), null, 'https://twitter.com', 1);
