9f3772b00e0eb382aa0ce38db9c8f309bbe05ad5
Vlasov Artem <avlasov@selectica.com>
2017-07-15 22:45:17 +0300

    Add MessageRepository
    Add MessageRepository and MessageRepositoryTest

---
+++ src/main/java/org/avlasov/messenger/repository/MessageRepository.java
@@ -0,0 +1,22 @@
+package org.avlasov.messenger.repository;
+
+import org.avlasov.messenger.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by artemvlasov on 15/07/2017.
+ */
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+    Optional<Message> findByTitle(String title);
+    List<Message> findByTitleLike(String title);
+    List<Message> findByBodyLike(String body);
+    @Query("SELECT u.messages FROM User u WHERE u.id = ?1")
+    List<Message> findAuthorMessages(int authorId);
+    boolean existsByTitle(String title);
+
+}
---
+++ src/test/java/org/avlasov/messenger/repository/MessageRepositoryTest.java
@@ -0,0 +1,83 @@
+package org.avlasov.messenger.repository;
+
+import org.avlasov.messenger.entity.Message;
+import org.hamcrest.collection.IsCollectionWithSize;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by artemvlasov on 15/07/2017.
+ */
+public class MessageRepositoryTest extends MainRepositoryTest {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Test
+    public void findByTitle_WithExistingTitle_ReturnMessageOptional() throws Exception {
+        Optional<Message> message = messageRepository.findByTitle("Message to Jane Doe");
+        assertTrue(message.isPresent());
+    }
+
+    @Test
+    public void findByTitle_WithNotExistingTitle_ReturnEmptyOptional() throws Exception {
+        Optional<Message> message = messageRepository.findByTitle("Missing title");
+        assertFalse(message.isPresent());
+    }
+
+    @Test
+    public void findByTitleLike_WithMatchingTitle_ReturnMessagesCollection() throws Exception {
+        List<Message> messages = messageRepository.findByTitleLike("%Message%");
+        assertFalse(messages.isEmpty());
+        assertThat(messages, IsCollectionWithSize.hasSize(2));
+    }
+
+    @Test
+    public void findByTitleLike_WithNotMatchingTitle_ReturnEmptyCollection() throws Exception {
+        List<Message> messages = messageRepository.findByTitleLike("Test");
+        assertTrue(messages.isEmpty());
+    }
+
+    @Test
+    public void findByBodyLike_WithMatchingBody_ReturnMessagesCollection() throws Exception {
+        List<Message> messages = messageRepository.findByBodyLike("%Jane Doe%");
+        assertFalse(messages.isEmpty());
+        assertThat(messages, IsCollectionWithSize.hasSize(1));
+    }
+
+    @Test
+    public void findByBodyLike_WithNotMatchingBody_ReturnEmptyCollection() throws Exception {
+        List<Message> messages = messageRepository.findByBodyLike("Test");
+        assertTrue(messages.isEmpty());
+    }
+
+    @Test
+    public void findAuthorMessages_WithExistingAuthorId_ReturnMessagesCollection() throws Exception {
+        List<Message> authorMessages = messageRepository.findAuthorMessages(1);
+        assertThat(authorMessages, IsCollectionWithSize.hasSize(1));
+    }
+
+    @Test
+    public void findAuthorMessages_WithNotExistingAuthorId_ReturnEmptyCollection() throws Exception {
+        List<Message> authorMessages = messageRepository.findAuthorMessages(999);
+        assertThat(authorMessages, IsCollectionWithSize.hasSize(0));
+    }
+
+    @Test
+    public void existsByTitle_WithExistingTitle_ReturnTrue() throws Exception {
+        assertTrue(messageRepository.existsByTitle("Message to Jon Doe"));
+    }
+
+    @Test
+    public void existsByTitle_WithNotExistingTitle_ReturnFalse() throws Exception {
+        assertFalse(messageRepository.existsByTitle("TEST"));
+    }
+
+}
